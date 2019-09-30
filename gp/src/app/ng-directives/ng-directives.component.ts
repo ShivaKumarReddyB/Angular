29da76638682f4b14c40c9834fd9b0a5cafcb5fd
8977shiva <b.skreddy1997@gmail.com>
2019-09-30 23:25:18 +0530

    bootstrap

--- gp/src/app/ng-directives/ng-directives.component.ts
+++ gp/src/app/ng-directives/ng-directives.component.ts
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ng-directives.component.css']
 })
 export class NgDirectivesComponent implements OnInit {
+     phone: any[] =  ['asus', 'iphone', 'nokia', 'mi'];
+      num:number = 0;
 
+      people: any[] = [
+        { name: 'Anderson', age: 35, city: 'Sao Paulo' },
+         { name: 'John', age: 12, city: 'Miami' },
+        { name: 'Peter', age: 22, city: 'New York' }
+        ];
+
+
+
+
+
+
+
+
+
+   working: boolean = true;
+
+   changeOnClick() {
+      this.working = ! this.working;
+   }
   constructor() { }
 
   ngOnInit() {
