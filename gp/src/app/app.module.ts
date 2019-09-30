289866114ccb7634cf27103c1b379c093647ee98
8977shiva <b.skreddy1997@gmail.com>
2019-09-30 21:49:03 +0530

    new

---
+++ gp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgDirectivesComponent } from './ng-directives/ng-directives.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NgDirectivesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ gp/src/app/ng-directives/ng-directives.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-directives',
+  templateUrl: './ng-directives.component.html',
+  styleUrls: ['./ng-directives.component.css']
+})
+export class NgDirectivesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
