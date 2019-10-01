7b128e2b97bbc151bf157fe7faba3bcb5cc74c0c
8977shiva <b.skreddy1997@gmail.com>
2019-10-02 01:21:46 +0530

    forms

--- gp/src/app/app.module.ts
+++ gp/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// importing formsmodule
+import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgDirectivesComponent } from './ng-directives/ng-directives.component';
+import { FormsComponent } from './forms/forms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NgDirectivesComponent
+    NgDirectivesComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
