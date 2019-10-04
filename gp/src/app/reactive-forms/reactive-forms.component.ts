b19df7ffcbaf5eb20716f02f1d26c93d4818a24b
8977shiva <b.skreddy1997@gmail.com>
2019-10-04 21:11:24 +0530

    project copy

--- gp/src/app/reactive-forms/reactive-forms.component.ts
+++ gp/src/app/reactive-forms/reactive-forms.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Customer } from './customer';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive-forms.component.css']
 })
 export class ReactiveFormsComponent implements OnInit {
+  customer = new Customer();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  save(customerForm: NgForm) {
+    console.log(customerForm.form);
+    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  }
 }
