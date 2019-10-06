d55ed7722305fbbd713e20c3db510e89c0b91b87
8977shiva <b.skreddy1997@gmail.com>
2019-10-06 10:27:30 +0530

    adding elements in array

--- gp/src/app/todo-list/todo-list.component.ts
+++ gp/src/app/todo-list/todo-list.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+  add: any;
+
+ array: any[] = [];
+ onclick() {
+   this.array.push(this.add)
+
+}
+onKeydown(event){
+  this.array.push(this.add)
+  console.log(event)
+}
+
 
   constructor() { }
 
